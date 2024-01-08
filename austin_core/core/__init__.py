cb935cdc8f32cee8c0d04da154534c2cd145f7f9
Austin <fairylandhost@outlook.com>
2024-01-08 22:12:38 +0800

    :bug: import

--- austin_core/core/__init__.py
+++ austin_core/core/__init__.py
@@ -19,13 +19,7 @@ warnings.filterwarnings("ignore")
 if platform.system() == "Windows":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-__name__ = "austin-core"
-__version__ = "0.0.1"
+from .Base import Base
+from .DefaultData import DefaultData
 
-from .core import Base
-from .core import DefaultData
-
-__all__ = [
-    "Base",
-    "DefaultData",
-]
+__all__ = ["Base", "DefaultData"]
